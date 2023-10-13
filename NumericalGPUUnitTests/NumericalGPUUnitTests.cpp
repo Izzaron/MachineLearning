13d733e52f598e36ad2c655bd74a04627a98d293
Gustafs PC <gustaf.holst87@gmail.com>
2023-10-13 21:47:59 +0200

    Added native unit test project

---
+++ NumericalGPUUnitTests/NumericalGPUUnitTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NumericalGPUUnitTests
+{
+	TEST_CLASS(NumericalGPUUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
